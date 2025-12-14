3be03385611dcb2132202be1b47a0a10fb0d5fa9
Marcel Marnitz <marcelmarnitz@googlemail.com>
2025-12-14 14:41:04 +0100

    Dependencies updated

---
+++ src/Controller/Autoconfig/AutoconfigController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller\Autoconfig;
+
+use SchulIT\CommonBundle\Controller\AutoconfigController as BaseAutoconfigController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/autoconfig')]
+class AutoconfigController extends BaseAutoconfigController {
+
+}
